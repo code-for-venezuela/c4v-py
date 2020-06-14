f34d92e42507da918895645f520d829d31b869c6
dieko95 <diegog2021@gmail.com>
2020-06-13 20:35:59 -0400

    added parse_args - bug
    The following error appears when the last line is executed in the terminal:
    
    data_sampler.py: error: unrecognized arguments: --path ../data_analysis/tagging-set-original_for_jupyter_tagging.csv --sample_n 10 --rand_state 19 --text_col full_text

--- helper_functions/data_sampler.py
+++ helper_functions/data_sampler.py
@@ -3,6 +3,7 @@ import sys
 import csv
 from datetime import datetime
 import getopt
+import argparse
 
 class data_sampler:
 
@@ -130,11 +131,43 @@ python data_sampler.py ../data_analysis/tagging-set-original_for_jupyter_tagging
     # opts, args = getopt.getopt(sys.argv, 'path:sample_n:random_sample:text_col:annotator_name')
     # print(sys.argv[1:])
 
+    def check_input():
+        parser = argparse.ArgumentParser(description='Path to csv file where tweets are located')
+        parser.add_argument('--path',
+                            type=str,
+                            help= 'Path to csv file where tweets are located')
+        
+        parser = argparse.ArgumentParser(description='Sample size to extract.')
+        parser.add_argument('--sample_size',
+                            type=int,
+                            help= 'Sample size to extract.')
 
-    data_sampler(tweets_dataset_path = str(sys.argv[1]), 
-                    sample_n = int(sys.argv[2]), 
-                    random_state = int(sys.argv[3]), 
-                    text_col = str(sys.argv[4]), 
-                    annotator_name = str(sys.argv[5]))
+        parser = argparse.ArgumentParser(description='Random State to use in the models')
+        parser.add_argument('--rand_state',
+                            type=int,
+                            help= 'Random State to us ein the models')
+        
+        parser = argparse.ArgumentParser(description='Column that contains raw tweets text')
+        parser.add_argument('--text_col',
+                            type=str,
+                            help= 'Column that contains rwe tweets text')
+        
+        parser = argparse.ArgumentParser(description='Name of the person that is annotating')
+        parser.add_argument('--annotator_name',
+                            type=str,
+                            help= 'Name of the person that is annotating')
+        
+        args = parser.parse_args()
+        return vars(args)
+
+    args = check_input()
+    # print(args)
 
 
+    data_sampler(tweets_dataset_path = args['path'], 
+                        sample_n = args['sample_size'], 
+                        random_state = args['rand_state'], 
+                        text_col = args['text_col'], 
+                        annotator_name = args['annotator_name'])
+
+#  python data_sampler.py --path ../data_analysis/tagging-set-original_for_jupyter_tagging.csv --sample_n 10 --rand_state 19 --text_col full_text --annotator_name diego
