0e3a2e6057f5cf5a3c6b74f1c6570996001f5b8e
dieko95 <diegog2021@gmail.com>
2020-06-13 21:03:24 -0400

    Dev diego/data sampler.py (#8)
    * added parse_args - bug
    
    The following error appears when the last line is executed in the terminal:
    
    data_sampler.py: error: unrecognized arguments: --path ../data_analysis/tagging-set-original_for_jupyter_tagging.csv --sample_n 10 --rand_state 19 --text_col full_text
    
    * data_sampler parameters fixed, second iteration dataset created

--- helper_functions/data_sampler.py
+++ helper_functions/data_sampler.py
@@ -3,6 +3,7 @@ import sys
 import csv
 from datetime import datetime
 import getopt
+import argparse
 
 class data_sampler:
 
@@ -16,6 +17,7 @@ class data_sampler:
         self.random_state = random_state
         self.text_col = text_col
         self.annotator_name = annotator_name
+        self.filename = None
         self.get_sample()
 
     def cleaner(self, df,
@@ -29,7 +31,7 @@ class data_sampler:
             df: Dataframe or Pandas.Series object. 
             text_col: String. Column to clean. 
             is_pandas_series: Boolean, Optional. If df is pandas.Series
-        
+
         '''
         
         # to lower
@@ -107,8 +109,8 @@ class data_sampler:
 
         timestamp = datetime.now()
         formatted_timestamp = timestamp.strftime('%Y-%m-%d_%H:%M:%S')
-        filename = f'tobrat-{self.annotator_name}-sample_n_{self.sample_n}-random_state_{self.random_state}---{formatted_timestamp}.csv'
-        original_df.to_csv(f'../data/data_to_annotate/{self.annotator_name}/{filename}',
+        self.filename = f'tobrat-{self.annotator_name}-sample_n_{self.sample_n}-random_state_{self.random_state}---{formatted_timestamp}.csv'
+        original_df.to_csv(f'../data/data_to_annotate/{self.annotator_name}/{self.filename}',
                             sep = ' ',
                             header = False,
                             index = False,
@@ -123,18 +125,56 @@ if __name__ == '__main__':
     '''
     Example:
 
-python data_sampler.py ../data_analysis/tagging-set-original_for_jupyter_tagging.csv 10 19 full_text diego
+python data_sampler.py --path ../data_analysis/tagging-set-original_for_jupyter_tagging.csv --sample_size 30 --rand_state 19 --text_col full_text --annotator_name diegoo
     '''
      
-    ## command line arguments to be implemented
-    # opts, args = getopt.getopt(sys.argv, 'path:sample_n:random_sample:text_col:annotator_name')
-    # print(sys.argv[1:])
+
+    def check_input():
+        parser = argparse.ArgumentParser(description='''
+        Sample tweets dataframe for annotations in brat.
+        
+        Example:
+            python data_sampler.py --path ../data_analysis/tagging-set-original_for_jupyter_tagging.csv --sample_size 30 --rand_state 19 --text_col full_text --annotator_name diego
+        ''')
+        parser.add_argument('--path',
+                            type=str,
+                            help= 'Path to csv file where tweets are located')
+        
+        parser.add_argument('--sample_size',
+                            type=int,
+                            help= 'Sample size to extract.')
+
+        parser.add_argument('--rand_state',
+                            type=int,
+                            help= 'Random State to used in the models')
+        
+        parser.add_argument('--text_col',
+                            type=str,
+                            help= 'Column that contains rwe tweets text')
+        
+        parser.add_argument('--annotator_name',
+                            type=str,
+                            help= 'Name of the person that is annotating')
+        
+        args = parser.parse_args()
+        return vars(args)
+
+    args = check_input()
+    # print(args)
 
 
-    data_sampler(tweets_dataset_path = str(sys.argv[1]), 
-                    sample_n = int(sys.argv[2]), 
-                    random_state = int(sys.argv[3]), 
-                    text_col = str(sys.argv[4]), 
-                    annotator_name = str(sys.argv[5]))
+    sample_data = data_sampler(tweets_dataset_path = args['path'], 
+                        sample_n = args['sample_size'], 
+                        random_state = args['rand_state'], 
+                        text_col = args['text_col'], 
+                        annotator_name = args['annotator_name'])
 
+    print(f'''
+    Success!
 
+    Data was saved at ../data/data_to_annotate/{sample_data.annotator_name}/{sample_data.filename}
+
+    '''
+    )
+
+#  python data_sampler.py --path ../data_analysis/tagging-set-original_for_jupyter_tagging.csv --sample_size 30 --rand_state 19 --text_col full_text --annotator_name diego
