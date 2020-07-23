cdc6f511aaba19d63ed39fbf1c704fa5a826cb71
Marianela Crissman <marianela_min@yahoo.com>
2020-07-23 10:42:34 -0500

    fixed path to data

--- tests/data/test_baseline_models.py
+++ tests/data/test_baseline_models.py
@@ -6,23 +6,21 @@ def test_baseline():
     '''
     The code within the __main__ might need to be added in the jupyter notebook to make use of the classes
     '''
-    print('hello')
 
     # -------------
     # Show me the performance of the models using annotated data
     file_names = [
-        # '../../brat-v1.3_Crunchy_Frog/data/first-iter/sampled_58_30',
-        '../../brat-v1.3_Crunchy_Frog/data/first-iter/balanced_dataset_brat',
-        '../../brat-v1.3_Crunchy_Frog/data/second-iter/diego-sample_30-randstate_19-2020-06-15_202334',
-        '../../brat-v1.3_Crunchy_Frog/data/second-iter/marianela-sample_50-randstate_42-2020-06-13_195818',
-        '../../brat-v1.3_Crunchy_Frog/data/second-iter/marianela-sample_50-randstate_42-2020-06-28_093100'
+        # 'data/processed/brat/sampled_58_30',
+        'data/processed/brat/balanced_dataset_brat',
+        'data/processed/brat/diego-sample_30-randstate_19-2020-06-15_202334',
+        'data/processed/brat/marianela-sample_50-randstate_42-2020-06-13_195818',
+        'data/processed/brat/marianela-sample_50-randstate_42-2020-06-28_093100'
     ]
 
     # -------------
     # shows me which tweets were ignored by the DataLoader class
     for pair in file_names:
         unannotated_tweets(pair)
-        print('hello')
 
     # ANALYZER USAGE
 
