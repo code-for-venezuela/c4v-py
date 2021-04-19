7e8a05eaf0abb5a31e591767b2ab6311c3dd9161
Luis Diaz <ldiazn98@gmail.com>
2021-04-19 12:29:05 -0400

    renamed module in settings file

--- src/scraper/settings.py
+++ src/scraper/settings.py
@@ -2,7 +2,7 @@
     This file manages multiple settings shared across the scrapper,
     such as mappings from urls to spiders
 """
-import scrapper.spiders as spiders
+import scraper.spiders as spiders
 
 # Dict with information to map from domain to 
 # Spider
