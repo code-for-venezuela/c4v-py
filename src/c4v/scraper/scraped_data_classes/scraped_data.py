d4502d286aa26f5aadd1ca26a9c96cc9e6286d28
Luis Diaz <ldiazn98@gmail.com>
2021-04-30 17:40:06 -0400

    changed misleading ScrapedData doc

--- src/c4v/scraper/scraped_data_classes/scraped_data.py
+++ src/c4v/scraper/scraped_data_classes/scraped_data.py
@@ -5,8 +5,14 @@ from typing import List
 @dataclass(frozen=True)
 class ScrapedData:
     """
-        Data class that maps directly to a database schema. Every scraper
-        should return its own instance of this class
+        This is a general data format class, 
+        every data format for other scrapers could have 
+        additional fields according to its needs and
+        scrapable data, but then they should be able to 
+        convert themselves into this format, possibly leaving a 
+        few fields as None. Thus, we can be able to 
+        easily map from a scrapers's output to a database 
+        scheme
     """
 
     title: str = None
