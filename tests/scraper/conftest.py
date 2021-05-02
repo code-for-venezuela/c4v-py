512d88fe92fc65ee82cd5731c7e2ee4c5cd757b7
Marianela Crissman <marianela_min@yahoo.com>
2021-05-02 14:45:53 -0500

    Wip - give better structure to json test file

--- tests/scraper/conftest.py
+++ tests/scraper/conftest.py
@@ -1,13 +1,20 @@
+import os
 import configparser
-
 import json
 import pytest
 
+from c4v.scraper.settings import C4V_ROOT_DIR
+
+TEST_SCRAPER_CONSTANTS = os.path.join(
+    C4V_ROOT_DIR, "resources/tests/scraper/test_scraper_constants.json"
+)
+
 
 @pytest.fixture
-def get_body_for_parse_ok():
-    filename = "../../resources/tests/scraper/test_scraper_constants.json"
-    text_of_interest = _get_json_from_file(filename)['el-pitazo']['get-body-parse-ok']
+def get_body_for_parse_ok_el_pitazo():
+    text_of_interest = _get_json_from_file(TEST_SCRAPER_CONSTANTS)["el-pitazo"][
+        "get-body-parse-ok"
+    ]
     return text_of_interest
 
 
@@ -25,6 +32,6 @@ def _get_properties_from_file(filename):
 
 # not needed, erase
 def _get_text_from_file(filename):
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         content = f.read()
     return content
