854d8fe6dc5619036a3fb3b4ceb36209982b489c
Luis Diaz <ldiazn98@gmail.com>
2021-05-19 14:01:48 -0400

    added self throtling config fields to scrapy settings

--- src/c4v/scraper/scrapy_settings.py
+++ src/c4v/scraper/scrapy_settings.py
@@ -3,4 +3,8 @@
 """
 
 # Settings passed to the crawler
-CRAWLER_SETTINGS = {"LOG_ENABLED": False}
+CRAWLER_SETTINGS = {
+    "LOG_ENABLED": False,
+    "ROBOTSTXT_OBEY" : True,
+    "DOWNLOAD_DELAY" : 0.25
+}
