98c08851efdec5b0108a1204462a60daa1573204
Luis Diaz <ldiazn98@gmail.com>
2021-06-03 15:49:08 -0400

    finished local scrape command

--- src/c4v/c4v_cli.py
+++ src/c4v/c4v_cli.py
@@ -6,8 +6,11 @@
 import click
 
 # Python imports
-from typing import List
+from typing import DefaultDict, List
+import os
+
 # Local imports
+from c4v.scraper.scraped_data_classes.scraped_data import ScrapedData
 from c4v.scraper.scraper import bulk_scrape
 
 @click.group()
@@ -21,18 +24,22 @@ def c4v_cli():
 @click.option('--files', is_flag=True, help="Interpret url list as files instead of urls, so urls are retrieved from such files")
 @click.option('--output', default=None, help="Output file to store results")
 @click.option('--max-len', default=-1, help="Truncate scraped content's body to a maximum lenght if provided.")
+@click.option('--pretty', is_flag=True, help="Print data formatted as human readable")
 @click.argument('urls', nargs = -1)
-def scrape(urls : List[str] = None, files : bool = None, output : str = None, max_len : int = -1):
+def scrape(urls : List[str] = None, files : bool = None, output : str = None, max_len : int = -1, pretty : bool = False):
     """
         Use this command to run a scraping process.
         Parameters:
             + urls : [str] = List of urls or files to get urls from
             + files : bool = if urls are in files or not, defaults to false
             + output : str = where to store output 
+            + max_len : int = max body size to print when pretty printing. Defaults to max size possible when not provided
+            + pretty : bool = If print should be human readable
     """
 
     # Check for errors
-    if not urls:
+    #   check if urls were provided
+    if not urls: 
         click.echo("No urls to scrape were provided")
         return
 
@@ -40,30 +47,36 @@ def scrape(urls : List[str] = None, files : bool = None, output : str = None, ma
     urls_to_scrape = []
     if files: # if urls are stored in files
         for file in urls: # iterate over every file
-            with open(file) as urls_file: 
-                content = map(lambda s: s.strip(), urls_file.readlines()) # parse every line as a single url
-                urls_to_scrape.extend(content)
+            try:
+                with open(file) as urls_file: 
+                    content = map(lambda s: s.strip(), urls_file.readlines()) # parse every line as a single url
+                    urls_to_scrape.extend(content)
+            except IOError as e:
+                click.echo(f"Could not open input file: {file}. Error: {e.strerror}")
     else:
         urls_to_scrape = urls
 
     # scrape urls 
     scraped_data = bulk_scrape(urls_to_scrape)
 
+    # Choose print function 
+    if pretty:
+        print_func = lambda x: x.pretty_print(max_len)
+    else:
+        print_func = lambda x: str(x)
+
     # Write output
     if output:
         # try print content into file if output file was provided 
         try:
-            with open(output, "rw+") as file:
+            with open(output, "w+") as file:
                 for data in scraped_data:
-                    print(data, file=file)
+                    print(print_func(data), file=file)
         except IOError as e:
             click.echo(f"Could not open {output} file. Error: {e.strerror}")
     else:
         for data in scraped_data:
-            if max_len < 0:
-                click.echo(data.pretty_print())
-            else:
-                click.echo(data.pretty_print(max_content_len=max_len))
+            click.echo(print_func(data))
 
 if __name__ == "__main__":
     c4v_cli()
