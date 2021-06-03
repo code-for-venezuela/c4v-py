6e6afb88179269f09b7771d398e722950e458eb9
Luis Diaz <ldiazn98@gmail.com>
2021-06-03 18:02:43 -0400

    added crawl command

--- src/c4v/c4v_cli.py
+++ src/c4v/c4v_cli.py
@@ -6,12 +6,12 @@
 import click
 
 # Python imports
-from typing import DefaultDict, List
+from typing import List
 import os
 
 # Local imports
-from c4v.scraper.scraped_data_classes.scraped_data import ScrapedData
 from c4v.scraper.scraper import bulk_scrape
+from c4v.scraper.settings import INSTALLED_CRAWLERS
 
 @click.group()
 def c4v_cli():
@@ -77,6 +77,75 @@ def scrape(urls : List[str] = None, files : bool = None, output : str = None, ma
     else:
         for data in scraped_data:
             click.echo(print_func(data))
+
+
+
+@c4v_cli.command()
+@click.option('--list', is_flag=True, help="list available crawlers")
+@click.option('--all', is_flag=True, help="Run all available crawlers")
+@click.option('--all-but', is_flag=True, help="Run all crawlers except listed ones")
+@click.option('--output', default=None, help="Store output in provided file")
+@click.argument('crawlers', nargs=-1)
+def crawl(crawlers : List[str] = [], list : bool = False, all : bool = False, all_but : bool = False, output : str = None):
+    """
+        Start a crawling process 
+    """
+    crawlable_sites = "".join([f"\t{crawl.name}\n" for crawl in INSTALLED_CRAWLERS])
+    
+    # list available crawlers if requested to 
+    if list:
+        click.echo(f"Crawlable sites: \n{crawlable_sites}")
+        if not crawlers and not all: # if nothing else to do, just end
+            return
+
+    # Check for errors:
+    # if no crawlers to use, just end
+    if not crawlers and not all:
+        click.echo(f"Not crawlers provided. Available crawlers:\n{crawlable_sites}")
+        return
+
+    # raise a warnning if incompatible flags where provided
+    if all_but and all:
+        click.echo(f"[WARNING] --all and --all-but incompatible flags were provided, using only all")
+
+    # set up crawlers to run 
+    if all:
+        crawlers_to_run = INSTALLED_CRAWLERS
+    elif all_but:
+        crawlers_to_run = [crawler for crawler in INSTALLED_CRAWLERS if crawler.name not in crawlers]
+    else:
+        crawlers_to_run = [crawler for crawler in INSTALLED_CRAWLERS if crawler.name in crawlers]
+
+    # function to format crawled urls list 
+    format_url_list = lambda list: "".join([f"{s}\n" for s in list])
+
+    # if output file provided, crawl 
+    if output:
+        try:
+            with open(output, "w+") as file:
+                # create process function
+                def process(list : List[str]):
+                    print(format_url_list(list), file=file)
+                
+                # crawl urls
+                for crawler in crawlers_to_run:
+                    c = crawler()
+                    c.crawl_urls(process)
+
+            return
+        except IOError as e:
+            click.echo(f"[ERROR] Unable to open file: {output}. Error: {e.strerror}")
+            return
+
+    # if output file not specified, just print to stdio
+    for crawler in crawlers_to_run:
+            c = crawler()
+
+            def process(list : List[str]):
+                click.echo(format_url_list(list))
+
+            c.crawl_urls(process)
+    
 
 if __name__ == "__main__":
     c4v_cli()
