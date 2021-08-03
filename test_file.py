a34c67f2f58200a085ac4b16fd756865c9597ff9
Luis Diaz <ldiazn98@gmail.com>
2021-08-03 17:23:40 -0400

    removed temporary file

--- test_file.py
+++
@@ -1,7 +0,0 @@
-from c4v.classifier.classifier import ClassifierExperiment
-
-
-
-experiment = ClassifierExperiment("testing", "first_one")
-
-print(experiment.run(train_args={'num_train_epochs' : 3}))
