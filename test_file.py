d0434f8fe8f83f04413958644d9b98fcc183f153
Luis Diaz <ldiazn98@gmail.com>
2021-08-03 16:38:56 -0400

    added num_train_epochs field to test how it performs with more epochs

--- test_file.py
+++ test_file.py
@@ -4,4 +4,4 @@ from c4v.classifier.classifier import ClassifierExperiment
 
 experiment = ClassifierExperiment("testing", "first_one")
 
-print(experiment.run(train_args={'epochs' : 2}))
+print(experiment.run(train_args={'num_train_epochs' : 2}))
