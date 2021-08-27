ccf3a317348d824f1d5d7738f3589af4ff4b5888
Luis Diaz <ldiazn98@gmail.com>
2021-08-27 13:32:03 -0400

    removed useless file

--- test_file.py
+++
@@ -1,6 +0,0 @@
-from c4v.classifier.classifier import ClassifierExperiment
-
-# branch name, experiment name
-experiment = ClassifierExperiment("testing", "first_one")
-
-print(experiment.run_experiment(train_args={'num_train_epochs' : 3}))
