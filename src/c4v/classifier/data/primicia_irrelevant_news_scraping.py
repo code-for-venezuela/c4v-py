430e06cb6b93105501b0aa89bd45b97115b4203f
Luis Diaz <ldiazn98@gmail.com>
2021-08-27 14:46:42 -0400

    added initial file for primicia scraping

---
+++ src/c4v/classifier/data/primicia_irrelevant_news_scraping.py
@@ -0,0 +1,7 @@
+"""
+    Use this script to scrape data from primicia with non-relevant information, 
+    and then store it in a csv file in its corresponding folder according to 
+    this cookiecutter template:
+    https://drivendata.github.io/cookiecutter-data-science/
+"""
+
