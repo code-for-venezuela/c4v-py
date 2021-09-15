d94cc5395b7185026acee5975294581df0cfe67b
Luis Diaz <ldiazn98@gmail.com>
2021-09-15 16:59:14 -0400

    added default language model to config

--- src/c4v/config.py
+++ src/c4v/config.py
@@ -9,7 +9,8 @@ settings = Dynaconf(
         Validator("DATE_FORMAT",            default="%Y-%m-%d %H:%M:%S.%f%z" ),
         Validator("C4V_FOLDER",             default=os.path.join(_HOME, ".c4v")),
         Validator("LOCAL_SQLITE_DB_NAME",   default="c4v_db.sqlite"),
-        Validator("LOCAL_SQLITE_DB",        default=None)
+        Validator("LOCAL_SQLITE_DB",        default=None),
+        Validator("DEFAULT_BASE_LANGUAGE_MODEL", default="BSC-TeMU/roberta-base-bne") # Base language model for the classifier
     ]
 )
 
