b07dddd3af00c8b88ec8282193c965046f797e16
Luis Diaz <ldiazn98@gmail.com>
2021-10-14 17:20:26 -0400

    added dotenv support in config file

--- src/c4v/config.py
+++ src/c4v/config.py
@@ -3,14 +3,15 @@ import os
 
 _HOME = os.environ.get("HOME")
 settings = Dynaconf(
-    envvar_prefix="DYNACONF",
-    settings_files=['./settings.toml', 'config/.secrets.toml'],
+    envvar_prefix="C4V",
+    settings_files=['settings.toml', '.secrets.toml'],
     validators= [
         Validator("DATE_FORMAT",            default="%Y-%m-%d %H:%M:%S.%f%z" ),
         Validator("C4V_FOLDER",             default=os.path.join(_HOME, ".c4v")),
         Validator("LOCAL_SQLITE_DB_NAME",   default="c4v_db.sqlite"),
         Validator("LOCAL_SQLITE_DB",        default=None)
-    ]
+    ],
+    load_dotenv=True
 )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
