085c2b0f2cc8d8e0bf3b0863d29add936e17afad
devops-c4v <some_email@email.com>
2021-10-26 20:34:01 +0000

    Update from commit: ${GITHUB_SHA} - configured pages branch and access token 0dde0e9ad2bfa396786b629dbd33561476dcf69f

--- brat-v1.3_Crunchy_Frog/data/to_brat/ann_creator.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for filename in *.txt
-do
-	filename=$(basename -- "$filename" .txt)
-	touch $filename.ann
-done
-
--- src/c4v/classifier/__init__.py
+++
@@ -1,3 +0,0 @@
-from c4v.classifier.classifier import Classifier
-from c4v.classifier.classifier_experiment import ClassifierExperiment, ClassifierArgs, ClassifierSummary
-from c4v.classifier.experiment import BaseExperiment, BaseExperimentArguments, BaseExperimentSummary
