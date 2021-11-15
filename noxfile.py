8d1c201ded1d07037e8085fabdc68488a2cdcdd6
Luis Diaz <ldiazn98@gmail.com>
2021-11-15 16:34:46 -0400

    added flag to install extra optional dependencies

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ import nox
     python=["3.8", "3.7", "3.6"],
     reuse_venv=True
 )
-def tests(session):
+def tests(session : nox.Session):
     args = session.posargs or ["--cov", "-m", "not e2e", "./tests"]
-    session.run("poetry", "install", external=True)
+    session.run("poetry", "install", "-E", "all", external=True )
     session.run("pytest", *args)
