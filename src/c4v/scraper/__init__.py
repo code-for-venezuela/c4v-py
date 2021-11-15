117cf40d7e7f25a66738f568a4ac4d07e27a1d5e
Luis Diaz <ldiazn98@gmail.com>
2021-11-15 16:17:00 -0400

    added missed import in scraper module

---
+++ src/c4v/scraper/__init__.py
@@ -0,0 +1,3 @@
+from c4v.scraper.scraper    import bulk_scrape, scrape
+from c4v.scraper.settings   import INSTALLED_CRAWLERS, SUPPORTED_DOMAINS, INSTALLED_SCRAPERS
+from c4v.scraper.scraped_data_classes.scraped_data import ScrapedData
