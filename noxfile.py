612e65f2c4239b46d2b086c245dc71c0a2431edd
Luis Diaz <ldiazn98@gmail.com>
2021-11-15 16:36:38 -0400

    removed type signature

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ import nox
     python=["3.8", "3.7", "3.6"],
     reuse_venv=True
 )
-def tests(session : nox.Session):
+def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e", "./tests"]
     session.run("poetry", "install", "-E", "all", external=True )
     session.run("pytest", *args)
