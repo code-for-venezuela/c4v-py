cef1d63c890d70727f77ae997257f4aa25ffd0e3
Luis Diaz <ldiazn98@gmail.com>
2021-12-03 16:31:41 -0400

    reverted bad git merge

--- src/c4v/microscope/manager.py
+++ src/c4v/microscope/manager.py
@@ -7,8 +7,8 @@ from c4v.scraper.persistency_manager.base_persistency_manager import (
 )
 from c4v.scraper.persistency_manager.sqlite_storage_manager import SqliteManager
 from c4v.scraper.scraped_data_classes.scraped_data import ScrapedData, Sources
-from c4v.scraper.scraper import bulk_scrape, _get_scraper_from_url
-from c4v.scraper.settings import INSTALLED_CRAWLERS
+from c4v.scraper.scraper import bulk_scrape, _get_scraper_from_url, scrape
+from c4v.scraper.settings import INSTALLED_CRAWLERS, SUPPORTED_DOMAINS
 from c4v.config import settings
 from c4v.microscope.metadata import Metadata
 import c4v.microscope.utils as utils
@@ -55,31 +55,59 @@ class Manager:
     def persistency_manager(self) -> BasePersistencyManager:
         return self._persistency_manager
 
+    def scrape(self, urls : Union[List[str], str] ) -> Union[List[ScrapedData], ScrapedData]:
+        """
+            Scrape the given url or set of urls and return its results
+        """
+        if not urls: # if nothing to process, return None
+            return None 
+        elif isinstance(urls, list) and urls and isinstance(urls[0], str):
+            return bulk_scrape(urls)
+        elif isinstance(urls, str):
+            return scrape(urls)
+        
+        raise TypeError(f"Expected argument should be a list of urls as strings or a single string url. Given {type(urls)}")
+
+    def crawl_and_scrape_for(self, crawler_names : List[str] = None, limit : int = -1, save_to_db = True) -> List[ScrapedData]:
+        """
+            Crawl and scrape data for the given list of crawlers
+            Parameters:
+                crawler_names : [str] = List of crawlers to use, defaults to all crawlers
+                limit : int = Maximum ammount of urls to scrape, set to negative number for no limit (no recommended)
+            Return:
+                List of scraped data    
+        """
+        urls = self.crawl_new_urls_for(crawler_names=crawler_names, limit=limit, save_to_db=save_to_db)
+        data = self.get_bulk_data_for(urls, save_to_db=save_to_db)
+        return data
+
     def get_bulk_data_for(
-        self, urls: List[str], should_scrape: bool = True, save: bool = True
+        self, urls: List[str], should_scrape: bool = True, save_to_db = True
     ) -> List[ScrapedData]:
         """
             Retrieve scraped data for given url set if scrapable
             Parameters:
                 urls : [str] = urls whose data is to be retrieved. If not available yet, then scrape it if requested so
-                should_scrape : bool = (optional) if should scrape non-existent urls
-                save : bool = (optional) if should save new data when scraped for the first time
+                should_scrape : bool = if should scrape non-existent urls
+                save_to_db : bool = if should save to db
+            Return:
+                List of gathered data, 
         """
         # just a shortcut
         db = self._persistency_manager
 
         # Separate scraped urls from non scraped
-        not_scraped = db.filter_scraped_urls(urls)
-
+        not_scraped = db.filter_scraped_urls(urls) if db else urls
+        
         # Scrape missing instances if necessary
+        items = []
         if should_scrape and not_scraped:
             items = bulk_scrape(not_scraped)
-            if save:
-                db.save(items)  # save if requested to
+            if save_to_db and db:
+                db.save(items)
 
-        # Convert to set to speed up lookup
         urls = set(urls)
-        return [sd for sd in db.get_all() if sd.url in urls]
+        return [sd for sd in db.get_all() if sd.url in urls] if db else items
 
     def get_data_for(self, url: str, should_scrape: bool = True) -> ScrapedData:
         """
@@ -98,6 +126,9 @@ class Manager:
             Parameters:
                 limit : int = how much measurements to scrape, set a negative number for no limit
         """
+        if not self._persistency_manager:
+            raise AttributeError("Storage scheme not configured for microscope Manager instance, provide a db scheme to use this function")
+
 
         db = self._persistency_manager
 
@@ -118,24 +149,31 @@ class Manager:
             Return:
                 An iterator returning available rows
         """
+        if not self._persistency_manager:
+            raise AttributeError("Storage scheme not configured for microscope Manager instance, provide a db scheme to use this function")
+
         return self._persistency_manager.get_all(limit, scraped)
 
-    def crawl_new_urls_for(
+    def crawl_and_process_new_urls_for(
         self,
         crawler_names: List[str] = None,
         post_process: Callable[[List[str]], None] = None,
         limit=-1,
-        save_to_db : bool = True
+        save_to_db = True
     ):
         """
             Crawl for new urls using the given crawlers only
             Parameters:
-                crawler_names : [str] = names of crawlers to be ran when this function is called. If no list is passed, then 
+                crawler_names : [str] = names of crawlers to be ran when this function is called. If no list is provided, then 
                                         all crawlers will be used
                 post_process : ([str]) -> None = Function to call over new elements as they come
-                limit        : int = Max amount of urls to save, -1 when no limit 
+                limit        : int = Max amount of urls to save
+                save_to_db : bool = If should save to local
         """
         db = self._persistency_manager
+        limit = limit if limit >= 0 else sys.maxsize
+
+        # set up limit if necessary
         limit = limit if limit >= 0 else sys.maxsize
 
         class Counter:
@@ -149,15 +187,17 @@ class Manager:
 
         # Function to process urls as they come
         def save_urls(urls: List[str]):
+
+            urls = urls[:limit - counter.count]
+
             if db:
-                # Filter already known urls and take at the must the necessary ones to fill the required size
-                urls = db.filter_known_urls(urls)[: limit - counter.count]
-                datas = [ScrapedData(url=url, source=Sources.SCRAPING) for url in urls]
+                urls = db.filter_known_urls(urls)
+                datas = [ScrapedData(url=url) for url in urls]
 
                 if save_to_db: db.save(datas)
 
             # Update how much elements have beed added so far
-            counter.add(len(datas))
+            counter.add(len(urls))
 
             # Call any callback function
             if post_process:
@@ -167,21 +207,43 @@ class Manager:
         def should_stop() -> bool:
             return counter.count >= limit
 
-        # Names for installed crawlers
-        crawlers = [c.name for c in INSTALLED_CRAWLERS]
-
         # if no list provided, default to every crawler
         if crawler_names == None:
-            crawler_names = crawlers
+            crawler_names = [c.name for c in INSTALLED_CRAWLERS]
 
         # Instantiate crawlers to use
         crawlers_to_run = [
             crawler() for crawler in INSTALLED_CRAWLERS if crawler.name in crawler_names
         ]
 
+        # Warns the user if no crawler could be found
+        if not crawlers_to_run:
+            given_crawler_list = "\n".join([f"\t* {c}" for c in crawler_names] or ["\t<No crawlers>"])
+            actual_crawler_list = "\n".join([f"\t* {c.name}" for c in INSTALLED_CRAWLERS] or ["\t<No crawlers>"])
+            print(f"[Warning] No crawler available to run. Given crawler list: \n{given_crawler_list}\n Available crawlers: \n{actual_crawler_list}", file=sys.stderr)
+
         # crawl for every crawler
         for crawler in crawlers_to_run:
             crawler.crawl_and_process_urls(save_urls, should_stop)
+
+    def crawl_new_urls_for( self,
+        crawler_names: List[str] = None,
+        limit=-1,
+        save_to_db : bool = True
+    ):
+        """
+            Crawl new urls for the given list of crawlers:
+            Parameters:
+                crawler_names : [str] = list of names of scrapers to use when crawling
+        """
+        items = []
+        
+        def collect_urls(urls : List[str]):
+            items.extend(urls)
+
+        self.crawl_and_process_new_urls_for(crawler_names=crawler_names, limit=limit, post_process=collect_urls, save_to_db=save_to_db)
+
+        return items
 
     def split_non_scrapable(self, urls: List[str]) -> Tuple[List[str], List[str]]:
         """
@@ -369,6 +431,21 @@ class Manager:
         from c4v.classifier.classifier import Classifier
         return Classifier.get_labels()
 
+    @staticmethod
+    def get_available_crawlers() -> List[str]:
+        """
+            List of usable names of crawlers
+        """
+        return [c.name for c in INSTALLED_CRAWLERS]
+
+    @classmethod
+    def scrapable_domains() -> List[str]:
+        """
+            List of scrapable domains, it may not match with the crawlable sites
+        """
+        return SUPPORTED_DOMAINS
+
+
     def should_retrain_base_lang_model(
         self,
         lang_model, #: LanguageModel,
@@ -422,3 +499,4 @@ class Manager:
         # Compute loss
         loss = lang_model.eval_accuracy(ds)
         return should_retrain_fn(loss)
+
