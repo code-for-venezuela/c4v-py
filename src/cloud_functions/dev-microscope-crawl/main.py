38a365b190c15c4a468174d904944fedefbb2029
Luis Diaz <ldiazn98@gmail.com>
2022-01-12 14:57:20 -0400

    added crawling function

--- src/cloud_functions/dev-microscope-crawl/main.py
+++ src/cloud_functions/dev-microscope-crawl/main.py
@@ -16,34 +16,30 @@ def crawl(request : flask.Request):
         function to trigger a crawling process in google cloud. 
         this function expects the following parameters through post request:
         # Parameters:
-            - `crawler_name` : str = name of the crawler to use while crawling
+            - `crawler_names` : [str] = list of crawlers to use while crawling
             - `limit` : int = (optional) Max ammount of urls to save, defaults to 1000
         # Environment Variables
             These are the expected environment variables that this function will try to use
             - `TABLE` : str = Name of the table in big query to use to store results
     """
+
     # Get logging objects
     logging_client = logging.Client()
     logger = logging_client.logger("microscope-crawl")
 
-    # Parse table name fron environment variables
-    table_name = os.environ.get("TABLE")
-    if not table_name:
-        logger.log_text("Not table name provided in environment variables", severity = "ERROR")
-        return "error"
-    
     # Parse config 
-    config = CrawlFuncConfig(table_name, request)
+    config = CrawlFuncConfig(request)
     if config.error:
         logger.log_text(config.error, severity="ERROR")
-        return "error"
+        print(config.error)
+        return flask.jsonify({"status":"error", "msg" : config.error})
 
     # Crawl new urls
-    logger.log_text(f"Crawling up to {config.limit} urls using crawler '{config.crawler_name}'")
-    crawled = config.manager.crawl_new_urls_for([config.crawler_name], config.limit)
-    logger.log_text(f"Crawled {len(crawled)} urls using crawler '{config.crawler_name}'")
+    logger.log_text(f"Crawling up to {config.limit} urls using crawler '{config.crawler_names}'")
+    crawled = config.manager.crawl_new_urls_for(config.crawler_names, config.limit)
+    logger.log_text(f"Crawled {len(crawled)} urls using crawler '{config.crawler_names}'")
     
-    return "success"
+    return flask.jsonify({"status" : "success", "crawled" : len(crawled), "crawler_name" : config.crawler_names})
 
 class CrawlFuncConfig:
     """
@@ -51,10 +47,22 @@ class CrawlFuncConfig:
     """
     DEFAULT_LIMIT : int = 100
 
-    def __init__(self, scraped_data_table_name : str, request : flask.Request) -> None:
+    def __init__(self, request : flask.Request) -> None:
 
         # Parse options from request
         request_json : Dict = request.get_json()
+
+         # Parse table name fron environment variables
+        table_name = os.environ.get("TABLE")
+        if not table_name:
+            self._error = "No table name provided in environment variables"
+            return
+
+        # Parse project id for firestore
+        project_id = os.environ.get("PROJECT_ID")
+        if not project_id:
+            self._error = "No PROJECT_ID  provided in environment variables"
+            return
 
         # Parse limit
         try:
@@ -64,20 +72,23 @@ class CrawlFuncConfig:
             return 
 
         # Parse crawler name 
-        self._crawler_name = request_json.get("crawler_name")
-        if not self._crawler_name:
+        self._crawler_names = request_json.get("crawler_name")
+        if not self.crawler_names:
             self._error = "Mandatory field 'crawler_name' not provided in request"
             return
+        elif not isinstance(self.crawler_names, str):
+            self._error = "Invalid value for 'crawler_names' arguments, should be a list of strings"
 
         # Check that crawler name is a valid crawler name
-        if not self.crawler_name in Manager.available_crawlers():
-            self._error = f"Crawler '{self.crawler_name}' not a valid crawler, available crawlers: {Manager.available_crawlers()}"
-            return
+        for crawler_name in self.crawler_names:
+            if not crawler_name in Manager.available_crawlers():
+                self._error = f"Crawler '{crawler_name}' not a valid crawler, available crawlers: {Manager.available_crawlers()}"
+                return
 
         # set up driver data
-        self._scrape_data_table_name = scraped_data_table_name
+        self._scrape_data_table_name = table_name
         client = CrawlFuncConfig.get_client()
-        self._manager = Manager(BigQueryManager(scraped_data_table_name, client))
+        self._manager = Manager(BigQueryManager(table_name, client, project_id))
 
         self._error = None
 
@@ -86,14 +97,14 @@ class CrawlFuncConfig:
         """
             Max ammount of urls to crawl, defaults to 100
         """
-        self._limit
+        return self._limit
 
     @property
-    def crawler_name(self) -> str:
+    def crawler_names(self) -> str:
         """
             Name of the crawler to be run
         """
-        self._crawler_name
+        return self._crawler_names
 
     @property
     def manager(self) -> Manager:
@@ -111,3 +122,5 @@ class CrawlFuncConfig:
     def get_client() -> bigquery.Client:
         client = bigquery.Client()
         return client 
+
+# crawl(FakeRequest({"crawler_name" : "primicia"}))
