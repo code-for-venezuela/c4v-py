5f31bce83c29d27174a9bb3010bbc13bf66be78b
Luis Diaz <ldiazn98@gmail.com>
2022-01-12 15:27:50 -0400

    better configuration for scrape function

--- src/cloud_functions/dev-microscope-scrape/main.py
+++ src/cloud_functions/dev-microscope-scrape/main.py
@@ -20,29 +20,24 @@ def scrape(request : flask.Request):
         # Environment Variables
             These are the expected environment variables that this function will try to use
             - `TABLE` : str = Name of the table in big query to use to store results
+            - `PROJECT_ID` : str = Name of the project to where to store firestore data
     """
     # Get logging objects
     logging_client = logging.Client()
     logger = logging_client.logger("microscope-scrape")
-
-    # Parse table name fron environment variables
-    table_name = os.environ.get("TABLE")
-    if not table_name:
-        logger.log_text("Not table name provided in environment variables", severity = "ERROR")
-        return "error"
     
     # Parse config 
-    config = ScrapeFuncConfig(table_name, request)
+    config = ScrapeFuncConfig(request)
     if config.error:
         logger.log_text(config.error, severity="ERROR")
-        return "error"
+        return {"status" : "error", "msg" : config.error} 
 
     # Crawl new urls
     logger.log_text(f"Scraping up to {config.limit} urls.")
-    crawled = config.manager.scrape_pending(limit=config.limit)
-    logger.log_text(f"Scraped {len(crawled)} urls.")
+    scraped = config.manager.scrape_pending(limit=config.limit)
+    logger.log_text(f"Scraped {len(scraped)} urls.")
     
-    return "success"
+    return {"status" : "success", "scraped" : len(scraped)} 
 
 class ScrapeFuncConfig:
     """
@@ -50,7 +45,19 @@ class ScrapeFuncConfig:
     """
     DEFAULT_LIMIT : int = 100
 
-    def __init__(self, scraped_data_table_name : str, request : flask.Request) -> None:
+    def __init__(self, request : flask.Request) -> None:
+
+        # Parse table name fron environment variables
+        table_name = os.environ.get("TABLE")
+        if not table_name:
+            self._error = "Not table name provided in environment variables"
+            return
+
+        # Parse project id for firestore
+        project_id = os.environ.get("PROJECT_ID")
+        if not project_id:
+            self._error = "No PROJECT_ID  provided in environment variables"
+            return
 
         # Parse options from request
         request_json : Dict = request.get_json()
@@ -63,9 +70,10 @@ class ScrapeFuncConfig:
             return 
 
         # set up driver data
-        self._scrape_data_table_name = scraped_data_table_name
+        self._scrape_data_table_name = table_name
+        self._project_id = project_id
         client = ScrapeFuncConfig.get_client()
-        self._manager = Manager(BigQueryManager(scraped_data_table_name, client))
+        self._manager = Manager(BigQueryManager(table_name, client, project_id))
 
         self._error = None
 
@@ -83,6 +91,10 @@ class ScrapeFuncConfig:
     @property
     def scrape_data_table_name(self) -> str:
         return self._scrape_data_table_name
+
+    @property
+    def project_id(self) -> str:
+        return self._project_id
 
     @property
     def error(self) -> str:
